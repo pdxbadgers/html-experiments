2de03cbee393a0e143afe004edc10ed10f0dcca1
Matt <elmatterino@users.noreply.github.com>
2015-10-15 22:13:44 -0700

    "Final" control app
    Signed-off-by: Matt <elmatterino@users.noreply.github.com>

--- code.js
+++ code.js
@@ -12,13 +12,67 @@ function ledSwitchChange(event, ui) {
     });
 }
 
+function enableIndividual(state) {
+    console.log("all "+state);
+    $("#switch_container").toggle(state);
+}
+
+
 function startup() {
     console.log("I'm a startup function");
     $("#badger-name").change(function(event) {
         var name = event.target.value;
-        $.ajax(urlBase+"/config?n="+name, {
+        $.ajax(urlBase+"/config?n="+encodeURIComponent(name), {
             success: function(data) {
             console.log("changed name succesfully");
+            }
+        });
+    });
+    $("#badger-flag").change(function(event) {
+        var flag = event.target.value;
+        $.ajax(urlBase+"/flag?newflag="+encodeURIComponent(flag), {
+            success: function(data) {
+            console.log("changed flag succesfully");
+            }
+        });
+    });
+    $("#nonebutton").on("click", function(event, ui) {
+        enableIndividual(true);
+        $.ajax(urlBase+"/leds?m=none", {
+            success: function(data) {
+                console.log("mode none");
+            }
+        });
+    });
+    $("#allbutton").on("click", function(event, ui) {
+        enableIndividual(false);
+        $.ajax(urlBase+"/leds?m=all", {
+            success: function(data) {
+                console.log("mode none");
+            }
+        });
+    });
+    $("#blinkbutton").on("click", function(event, ui) {
+        enableIndividual(false);
+        $.ajax(urlBase+"/leds?m=blink", {
+            success: function(data) {
+                console.log("mode none");
+            }
+        });
+    });
+    $("#chasebutton").on("click", function(event, ui) {
+        enableIndividual(false);
+        $.ajax(urlBase+"/leds?m=chase", {
+            success: function(data) {
+                console.log("mode none");
+            }
+        });
+    });
+    $("#twinklebutton").on("click", function(event, ui) {
+        enableIndividual(false);
+        $.ajax(urlBase+"/leds?m=twinkle", {
+            success: function(data) {
+                console.log("mode none");
             }
         });
     });
@@ -28,43 +82,60 @@ function startup() {
     $("#nose").change({d:3}, ledSwitchChange);
     $("#eye").change({d:4}, ledSwitchChange);
 
-    $("body").pagecontainer({
-        beforechange: function( event, ui ) {
-            var toPage = ui.toPage;
-            if (typeof(toPage) == 'string' && toPage.indexOf("#this-badger") > -1) {
-                $.ajax(urlBase+"/config", {
-                    success: function(data) {
-                        var obj = JSON.parse(data);
-                        $("#badger-name").attr("value", obj["n"]);
-                    }
-                });
+    var ledMap = {
+        0: "#tail",
+        1: "#back-foot",
+        2: "#front-foot",
+        3: "#nose",
+        4: "#eye"
+    };
+
+    $.ajax(urlBase+"/config", {
+        success: function(data) {
+            var obj = JSON.parse(data);
+            $("#badger-name").attr("value", decodeURIComponent(obj["n"]));
+        }
+    });
+    $.ajax(urlBase+"/leds", {
+        success: function(data) {
+            var obj = JSON.parse(data);
+            if (obj["m"]=="none") {
+                $('#nonebutton').addClass('ui-btn-active');
+                enableIndividual(true);
                 $.ajax(urlBase+"/leds/", {
                     success: function(data) {
                         var obj = JSON.parse(data);
+                        console.log("data!");
+                        console.log(data);
                         for (led in obj) {
-                            switch (led["d"]) {
-                                case 0:
-                                    if (led["s"]=="on") $("#tail").click();
-                                    break;
-                                case 1:
-                                    if (led["s"]=="on") $("#back-foot").click();
-                                    break;
-                                case 2:
-                                    if (led["s"]=="on") $("#front-foot").click();
-                                    break;
-                                case 3:
-                                    if (led["s"]=="on") $("#eye").click();
-                                    break;
-                                case 4:
-                                    if (led["s"]=="on") $("#eye").click();
-                                    break;
-                            }
+                            if (obj[led]["s"]=="on") $(ledMap[obj[led]["d"]]).click();
                         }
                     }
                 });
-
-                console.log("going to this badger");
             }
+            else {
+                enableIndividual(false);
+                switch (obj["m"]) {
+                    case "all":
+                        $('#allbutton').addClass('ui-btn-active');
+                        break;
+                    case "blink":
+                        $('#blinkbutton').addClass('ui-btn-active');
+                        break;
+                    case "chase":
+                        $('#chasebutton').addClass('ui-btn-active');
+                        break;
+                    case "twinkle":
+                        $('#twinklebutton').addClass('ui-btn-active');
+                        break;
+                }
+
+            }
+        }
+    });
+    $.ajax(urlBase+"/flag", {
+        success: function(data) {
+            $("#badger-flag").attr("value", decodeURIComponent(data));
         }
     });
 
